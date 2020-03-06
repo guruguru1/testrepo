ac656023491dc2aba3e4f8f805dee9e1b7dc0901
guru guru <guru@gu.ru>
2020-03-06 13:52:59 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# this is a readme
+
+Some more readme contents!
+I changed the readme!
---
+++ setup_ci.py
@@ -0,0 +1,8 @@
+import time
+
+if __name__ == '__main__':
+    print('building lib................')
+    time.sleep(10)
+    print('starting db...')
+    time.sleep(7)
+    print('done!')
