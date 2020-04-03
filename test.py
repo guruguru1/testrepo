53114668a8f5e0d8d9214848c55f6904487f406b
guruguru <guru@gu.ru>
2020-04-03 14:26:09 -0400

    edit test

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ class TestStringMethods(unittest.TestCase):
     def test_isupper(self):
         self.assertTrue('FOO'.isupper())
         self.assertFalse('Foo'.isupper())
-        print('hello')
+        print('hello2')
 
     def test_split(self):
         s = 'hello world'
